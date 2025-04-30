03559aaa8e787c760915114576e5ef63724b6a3c
Carlguz <71299737+Carlguz@users.noreply.github.com>
2025-04-29 22:37:15 -0500

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -1,8 +1,8 @@
 import requests
 
-topic = open("input/topics.txt").read().strip()
-img_url = f"https://source.unsplash.com/1280x720/?{topic}"
-img_data = requests.get(img_url).content
+# Imagen relacionada al tema de las abejas
+url = "https://source.unsplash.com/1280x720/?bee,honey"
 
+response = requests.get(url)
 with open("assets/images/bg.jpg", "wb") as f:
-    f.write(img_data)
+    f.write(response.content)
